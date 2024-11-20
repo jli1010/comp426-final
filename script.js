56130df942ad44863fd69d36f29ed4e0c433b15b
Teertha Penumatcha <teerthap@hotmail.com>
2024-11-20 12:51:46 -0500

    response returned to frontend

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document.getElementById('login-submit').addEventListener('click', () => {
     document.getElementById('login-box').remove();
     document.getElementById('login-display').innerHTML = username;
 })
+
+const submit_button = document.getElementById("submit-button");
+
+submit_button.addEventListener('click', async (e) => {
+    let input = document.getElementById('question').innerHTML;
+    let response = await fetch(`http://localhost:3000/eight_ball`);
+    let result = await response.json();
+    let output = document.getElementsByClassName('response')[0];
+    output.innerHTML = result.answer;
+    let gif_img = document.getElementById('image_response');
+    gif_img.src = result.image;
+})
